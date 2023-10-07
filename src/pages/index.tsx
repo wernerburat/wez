2d74801cb0050efbbd730c2eacb8359c960e77b2
Werner Burat <werner.burat.1@ens.etsmtl.ca>
2023-10-07 01:43:46 -0400

    updating

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,14 @@ const CreatePostWizard = () => {
 
   const [input, setInput] = useState("");
 
-  const { mutate } = api.posts.create.useMutation();
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) return null;
 
@@ -35,6 +42,7 @@ const CreatePostWizard = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
       <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
