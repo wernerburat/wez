175dee07e1f836e2e64d5e4727c28dfb86edd2c2
Werner Burat <werner.burat.1@ens.etsmtl.ca>
2023-10-06 22:15:04 -0400

    content test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,25 +21,19 @@ export default function Home() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              href="#"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
+              <h3 className="text-2xl font-bold">????</h3>
+              <div className="text-lg">Ayoye check ça</div>
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              href="#"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
+              <h3 className="text-2xl font-bold">ALLO?</h3>
+              <div className="text-lg">Quessé ça ben voyons là là là...</div>
             </Link>
           </div>
           <p className="text-2xl text-white">
