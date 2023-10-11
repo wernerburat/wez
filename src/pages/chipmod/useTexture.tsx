f41c8b83e20fea3487662ef042727053501ac722
Werner Burat <werner.burat.1@ens.etsmtl.ca>
2023-10-11 00:31:11 -0400

    might fix

--- src/pages/chipmod/useTexture.tsx
+++ src/pages/chipmod/useTexture.tsx
@@ -2,10 +2,12 @@ import { useRef } from "react";
 import { useScene } from "react-babylonjs";
 import { DynamicTexture } from "@babylonjs/core";
 
-export const useTexture = () => {
+const useTexture = () => {
   const textureRef = useRef<DynamicTexture>(
     new DynamicTexture("dt", { width: 512, height: 512 }, useScene(), false, 0),
   );
   textureRef.current.hasAlpha = true;
   return textureRef;
 };
+
+export default useTexture;
--- src/pages/chipmod/visualizers/VisType.tsx
+++ src/pages/chipmod/visualizers/VisType.tsx
@@ -1,11 +1,9 @@
 import { type Sound } from "@babylonjs/core";
 
-type VisualizerProps = {
+export type VisualizerProps = {
   soundRef: React.MutableRefObject<Sound | null>;
   showDebug?: boolean;
   bpm: number;
   offset: number;
   freqArray?: Uint8Array;
 };
-
-export default VisualizerProps;
